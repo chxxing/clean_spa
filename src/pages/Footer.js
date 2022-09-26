c3f4238efc5bb1d630affa1c1ffb3ecd7fe0d377
chxxing <choing26@naver.com>
2022-09-26 13:36:47 +0900

    220926

--- src/pages/Footer.js
+++ src/pages/Footer.js
@@ -1,11 +1,21 @@
 import React from 'react'
 
-const Footer = () => {
+const Footer = ({ content }) => {
     return (
         <footer className='Footer'>
-            Footer
+            <address>
+                {content.address}
+            </address>
+            <ul className="ft__con">
+                <li>사업자등록번호 : {content.number}</li>
+                <li>Tel : {content.tel}</li>
+                <li>e-mail : {content.email}</li>
+            </ul>
+            <span>
+                &copy; {content.name} All Rights Reserved.
+            </span>
         </footer>
     )
 }
 
-export default Footer;
+export default Footer
